c5ff7c3bfa65d10f7a21d6145b1f82a39f4ab362
Cole <122782745+cm-thom@users.noreply.github.com>
2023-06-09 00:41:18 -0400

    added routes for find single tag, product, category

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -12,10 +12,23 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
